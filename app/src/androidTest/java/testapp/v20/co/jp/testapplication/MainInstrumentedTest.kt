8f167761a0bb0994fd085d601da4051065114e0f
takaki <vsn2047508@gmail.com>
2019-06-04 14:54:31 +0900

    instrumented test add

---
+++ app/src/androidTest/java/testapp/v20/co/jp/testapplication/MainInstrumentedTest.kt
@@ -0,0 +1,31 @@
+package testapp.v20.co.jp.testapplication
+
+import android.support.test.runner.AndroidJUnit4
+import android.support.test.rule.ActivityTestRule
+import android.widget.TextView
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainInstrumentedTest {
+
+    private lateinit var message: TextView
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun _初期表示_MainActivity() {
+        // Context of the app under test.
+        message = mActivityTestRule.activity.findViewById(R.id.hello_world)
+        Assert.assertEquals("Hello World!", message.text)
+    }
+}
